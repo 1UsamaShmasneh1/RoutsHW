0d2615c6e28d8076f03343679c10d4f709a31194
usama shmasneh <us.05.07.91@gmail.com>
2022-06-07 21:50:30 +0300

    Add project files.

---
+++ RoutsHW/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutsHW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id}:min(5)")]
+        public string GetIntNumber(int id)
+        {
+            return "hello from int";
+        }
+
+        [Route("{id:regex(book(zoom|boom))}")]
+        public string GetIntString(string id)
+        {
+            return "hello from string";
+        }
+
+    }
+}
